8ced03d35566dbdf7d8ab042ec57aca7f9e49deb
Magali Ciccarone <magui_lm89@hotmail.com>
2023-09-23 19:31:58 -0300

    cambiando estilos

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
+# ONASHAGA EXPEDITIONS
 
+Las mejores excursiones para que puedas recorrer y conocer la ciudad más austral del mundo. No te lo podés perder.
+
+# PROYECTO FINAL
+
+Hola, mi nombre es Magalí, soy estudiante de Coderhouse y me encuentro realizando la carrera de Fullstack]. A continuación les presento la entrega final de mi proyecto para ReactJS. **Espero que lo disfruten tanto como yo!**.
+Onashaga expeditions se trata de una empresa de turismo, ubicada en la ciudad de Ushuaia, Tierra del Fuego. Podremos encontrar aquí, cada una de las excursiones que brinda dicha empresa dividas en tres secciones: Navegación, Trekking, y Tours. Si clickeamos cada excursión podremos ver el detalle de cada una y además tendremos la posibilidad de poder comprarla.
+
+## Pasos a seguir para levantar el proyecto:
+
+- Clonar el repositorio desde git hub
+
+```
+ git clone https://github.com/Maguiciccarone/EntregaFinalCiccarone.git
+```
+
+- Ir a la ruta del proyecto
+
+```
+cd onashaga-expeditions
+```
+
+- Una vez en la ruta...
+
+```
+npm install
+npm start
+```
+
+## DEMO
+
+A continuación podrás ver un breve resumen de como funciona este proyecto.
--- src/components/CheckOut/CheckOut.jsx
+++ src/components/CheckOut/CheckOut.jsx
@@ -8,28 +8,52 @@ import './Checkout.css';
 const Checkout = () => {
     const [orderId, setOrderId] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
+    const [formData, setFormData] = useState({
+        message: "",
+        email: "",
+        name: "",
+    });
     const { cart, clear } = useContext(CartContext);
 
     const total = getCartTotal(cart);
 
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleReset = () => {
+        setFormData({
+            name: "",
+            email: "",
+            message: "",
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        handleReset();
+    };
     const handleCheckout = () => {
         const order = {
-            buyer: {
-                name: "John",
-                phone: "123456789",
-                email: "john@gmail.com",
-            },
+            buyer: formData,
             items: mapCartToOrderItems(cart),
             total,
             date: serverTimestamp(),
         };
 
         setIsLoading(true);
-        createOrder(order).then((docRef) => {
-            setOrderId(docRef.id);
-            setIsLoading(false);
-            clear();
-        });
+        createOrder(order)
+            .then((docRef) => {
+                setOrderId(docRef.id);
+                setIsLoading(false);
+                clear();
+            });
     };
 
     return (
@@ -38,13 +62,47 @@ const Checkout = () => {
 
             <h4 style={{ color: "grey" }}>Resumen de la compra</h4>
 
-            {orderId && <p className="alert alert-success" role="alert">El id de la orden es: {orderId}</p>}
+            {orderId && <p class="alert alert-success" role="alert">El id de la orden es: {orderId}</p>}
 
             {!orderId && (
                 <>
                     <div>
-                        <h4>Formulario de contacto</h4>
+                        <p style={{ color: "grey" }}>Dejanos tus datos para poder brindarte el número de orden</p>
+                        <form className="container" onSubmit={handleSubmit}>
+                            <div className="form-contact">
+                                <label className="label-form" htmlFor="name">Nombre: </label>
+                                <input className="input-form"
+                                    type="text"
+                                    name="name"
+                                    id="name"
+                                    value={formData.name}
+                                    onChange={handleChange}
+                                />
+                            </div>
 
+                            <div className="form-contact">
+                                <label className="label-form" htmlFor="email">Email:</label>
+                                <input className="input-form"
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                />
+                            </div>
+
+                            <div className="form-contact">
+                                <label className="label-form" htmlFor="message">Mensaje: </label>
+                                <textarea className="input-form"
+                                    value={formData.message}
+                                    name="message"
+                                    id="message"
+                                    onChange={handleChange}
+                                />
+                            </div>
+
+                            <button className="btn btn-light mb-4" style={{ color: "#74b0bc", borderColor: "lightgrey" }} type="submit">Enviar</button>
+                        </form>
 
                     </div>
 
